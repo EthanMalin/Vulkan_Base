d6b9ad72308d3b71bb889faee776efe9ebfe0686
Ethan Malin <ethanmalin01@gmail.com>
2020-11-13 14:35:17 -0600

    ignoring build?

---
+++ build/res/shaders/compile.bat
@@ -0,0 +1,3 @@
+C:\dev\vulkan\1.2.135.0\Bin32\glslc.exe basic.vert -o vert.spv
+C:\dev\vulkan\1.2.135.0\Bin32\glslc.exe basic.frag -o frag.spv
+pause
---
+++ build/src/CMakeFiles/power.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/power"
+  "../bin/power.pdb"
+  "CMakeFiles/power.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/power.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
